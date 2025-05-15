674097cfc7ac04099d4cc3c14a38a61859d8e276
yohanserio <166560988+AntonioVinicius776@users.noreply.github.com>
2025-05-14 20:30:29 -0400

    Update caculadora.py
    ovo apagar essa merda

--- caculadora.py
+++ caculadora.py
@@ -1,18 +1,1 @@
-print("insira os dois numeros")
-n0=int(input())
-n1=int(input())
 
-esc0=int(input("o que deseja ?\n1-somar\n2-subtrair\n3-multiplicar\n4-dividir\n5-sair\n"))
-while esc0 !=5 :
-    esc0=int(input("o que deseja ?\n1-somar\n2-subtrair\n3-multiplicar\n4-dividir\n5-sair\n"))
-    continue
-if esc0==1:
-    print("soma", n0+n1)
-elif esc0==2:
-    print("sub", n0-n1)
-elif esc0==3:
-    print("multi", n0*n1)
-elif esc0==4:
-    print("div", n0/n1)
-elif esc0==5:
-    print("tenha um bom dia")
